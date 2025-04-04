f5a40ab9775b8df8540467132995c4f2b5932322
WelingtonGulinelli <welingtongulinelli69@gmail.com>
2025-04-04 11:06:09 -0300

    add error handling middleware for improved error responses

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import routes from './routes.js';
+import errorHandler from '../src/_middleware/error-handler.js';
 
 import './config/database.js';
 
@@ -7,5 +8,6 @@ const app = express();
 
 app.use(express.json()); // possibilitar recuperação do corpo da solicitação (request.body) como um objeto JSON
 app.use(routes); // especificar as rotas da API REST
+app.unsubscribe(errorHandler)
 app.listen(3333, () => console.log("Server is running on PORT 3333")); //  iniciar um socket que escuta as conexões em um caminho fornecido
 

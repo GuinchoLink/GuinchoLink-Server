0ac805379aaa638b6d87a6086db1d0335b05b2a7
EduardoAlmeidaBR03 <eduardo.almeida09@hotmail.com>
2025-06-16 11:18:28 -0300

    fix: Update production database configuration to use local settings

--- src/config/database-config.js
+++ src/config/database-config.js
@@ -27,10 +27,10 @@
 // Configuração do banco de dados no ambiente de produção
 export const databaseConfig = {
   dialect: 'postgres',
-  host: 'dpg-d11df3re5dus738l9dk0-a.oregon-postgres.render.com',
-  username: 'guincho_link_user',
-  password: 'mQcmT8oC34l9ncpn9VNRJsutqXIOLrZk',
-  database: 'guincho_link',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'postgres',
+  database: 'guincho-link',
   define: {
     timestamps: true,
     freezeTableName: true,
